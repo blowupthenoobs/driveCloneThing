a2fc1b3621e3c3ae406f5dba45c6beb1bf5de2ef
blowupthenoobs <jeremiah122508@gmail.com>
2025-11-24 11:56:27 -0500

    Tried new Thumbnail thing

--- backend/services/chunk-service/utils/createImageThumbnail.ts
+++ backend/services/chunk-service/utils/createImageThumbnail.ts
@@ -1,23 +1,12 @@
-import crypto from "crypto";
 import Thumbnail from "../../../models/thumbnail-model";
-import sharp from "sharp";
 import { FileInterface } from "../../../models/file-model";
 import { UserInterface } from "../../../models/user-model";
-import uuid from "uuid";
-import env from "../../../enviroment/env";
-import { createGenericParams } from "./storageHelper";
-import { S3Actions } from "../actions/S3-actions";
-import { FilesystemActions } from "../actions/file-system-actions";
 import { EventEmitter } from "stream";
 import FileDB from "../../../db/mongoDB/fileDB";
-import { getStorageActions } from "../actions/helper-actions";
-import { getFSStoragePath } from "../../../utils/getFSStoragePath";
-
 const fileDB = new FileDB();
 
-const storageActions = getStorageActions();
 
-const processData = (
+const processData = async (
   file: FileInterface,
   filename: string,
   user: UserInterface
@@ -25,87 +14,27 @@ const processData = (
   const eventEmitter = new EventEmitter();
 
   try {
-    const password = user.getEncryptionKey();
+    const thumbnailModel = new Thumbnail({
+      name: filename,
+      owner: user._id,
+      IV: file.metadata.IV,
+      path: file.metadata.filePath,
+      s3ID: file.metadata.s3ID,
+      originalFile: file._id
+    })
 
-    let CIPHER_KEY = crypto.createHash("sha256").update(password!).digest();
+    await thumbnailModel.save();
 
-    const thumbnailFilename = uuid.v4();
-
-    const thumbnailIV = crypto.randomBytes(16);
-
-    const params = createGenericParams({
-      filePath: file.metadata.filePath,
-      Key: file.metadata.s3ID,
-    });
-
-    const readStream = storageActions.createReadStream(params);
-
-    const { writeStream, emitter } = storageActions.createWriteStream(
-      params,
-      readStream,
-      thumbnailFilename
+    const updatedFile = await fileDB.setThumbnail(
+      file._id!.toString(),
+      thumbnailModel._id.toString()
     );
 
-    const decipher = crypto.createDecipheriv(
-      "aes256",
-      CIPHER_KEY,
-      file.metadata.IV
-    );
-
-    const imageResize = sharp().resize(300);
-
-    const handleFinish = async () => {
-      const thumbnailModel = new Thumbnail({
-        name: filename,
-        owner: user._id,
-        IV: thumbnailIV,
-        path: getFSStoragePath() + thumbnailFilename,
-        s3ID: thumbnailFilename,
-      });
-
-      await thumbnailModel.save();
-
-      const updatedFile = await fileDB.setThumbnail(
-        file._id!.toString(),
-        thumbnailModel._id.toString()
-      );
-
-      if (!updatedFile) {
-        throw new Error("Thumbnail Not Set");
-      }
-
-      eventEmitter.emit("finish", updatedFile);
-    };
-
-    const handleError = (e: Error) => {
-      eventEmitter.emit("error", e);
-    };
-
-    readStream.on("error", handleError);
-
-    writeStream.on("error", handleError);
-
-    decipher.on("error", handleError);
-
-    imageResize.on("error", handleError);
-
-    const thumbnailCipher = crypto.createCipheriv(
-      "aes256",
-      CIPHER_KEY,
-      thumbnailIV
-    );
-
-    readStream
-      .pipe(decipher)
-      .pipe(imageResize)
-      .pipe(thumbnailCipher)
-      .pipe(writeStream);
-
-    if (emitter) {
-      emitter.on("finish", handleFinish);
-    } else {
-      writeStream.on("finish", handleFinish);
+    if(!updatedFile) {
+      throw new Error("Thumbnail not set");
     }
+
+    eventEmitter.emit("finish", updatedFile);
   } catch (e) {
     eventEmitter.emit("error", e);
   }
